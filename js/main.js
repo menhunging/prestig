20cc11736db98bfe68841d33c2dd825244aad8a5
menhunging <menhunging@gmail.com>
2024-09-26 11:31:35 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,187 @@
+$(document).ready(function () {
+  if ($(".phone-input").length > 0) {
+    $(".phone-input").map(function () {
+      $(this).inputmask({
+        mask: "+7(999) 999-99-99",
+        placeholder: "*",
+        showMaskOnHover: false,
+        showMaskOnFocus: true,
+        clearIncomplete: true,
+      });
+    });
+  }
+
+  if ($("[data-fancybox]").length > 0) {
+    Fancybox.bind("[data-fancybox]", {
+      speedIn: 600,
+      speedOut: 600,
+    });
+  }
+
+  if ($(".slider-type").length > 0) {
+    const swiperTypes = new Swiper(".slider-type__slider", {
+      slidesPerView: 1,
+      spaceBetween: 200,
+      initialSlide: 2,
+      navigation: {
+        prevEl: ".slider-type__slider .swiperBtnPrev",
+        nextEl: ".slider-type__slider .swiperBtnNext",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        560: {
+          slidesPerView: 1,
+          spaceBetween: 50,
+        },
+        1280: {
+          slidesPerView: 1,
+          spaceBetween: 200,
+        },
+      },
+    });
+
+    $(".slider-type__names .services-page__container")
+      .stop()
+      .animate(
+        { scrollLeft: $(".slider-type__item.active").offset().left - 125 },
+        200
+      );
+
+    swiperTypes.on("slideChangeTransitionEnd", function () {
+      let margin = 140;
+
+      margin = margin * $(".slider-type__item.active").index();
+
+      $(".slider-type__names .services-page__container")
+        .stop()
+        .animate({ scrollLeft: margin - 125 }, 200);
+    });
+
+    swiperTypes.on("slideChange", function () {
+      $(".slider-type__item").removeClass("active");
+      $(".slider-type__item").eq(this.activeIndex).addClass("active");
+
+      $(".slider-type__invis").removeClass("opened");
+      $(document).off("mouseup");
+    });
+
+    if ($(".slider-type__item").length > 0) {
+      $(".slider-type__item").on("click", function () {
+        let index = $(this).index();
+        $(".slider-type__item").removeClass("active");
+        $(this).addClass("active");
+        swiperTypes.slideTo(index);
+      });
+    }
+
+    if ($(".slider-type__slider .circle").length > 0) {
+      $(".slider-type__slider .circle").on("click", function () {
+        let self = $(this);
+        let block = self.parents(".swiper-slide").find(".slider-type__invis");
+
+        block.addClass("opened");
+
+        $(document).mouseup(function (e) {
+          if (!block.is(e.target) && block.has(e.target).length === 0) {
+            block.removeClass("opened");
+            $(document).off("mouseup");
+          }
+        });
+      });
+    }
+  }
+
+  if ($(".slider-brands__slider").length > 0) {
+    const swiperTypes = new Swiper(".slider-brands__slider", {
+      slidesPerView: 5,
+      spaceBetween: 40,
+      loop: true,
+      watchSlidesProgress: true,
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        prevEl: ".slider-brands__slider .swiperBtnPrev",
+        nextEl: ".slider-brands__slider .swiperBtnNext",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1440: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+      },
+    });
+  }
+
+  // if ($(".products-slider").length > 0) {
+  //   let destroySliderProducts = () => {};
+
+  //   if ($(window).width() >= 768) {
+  //     $(".products-slider").addClass("destroy");
+  //     initSliderProducts();
+  //   } else {
+  //     $(".products-slider").addClass("destroy");
+  //   }
+
+  //   $(window).resize(function () {
+  //     if ($(window).width() < 768) {
+  //       destroySliderProducts();
+  //     } else {
+  //       initSliderProducts();
+  //     }
+  //   });
+
+  //   function initSliderProducts() {
+  //     if ($(".products-slider").hasClass("destroy")) {
+  //       $(".products-slider").removeClass("destroy");
+
+  //       const swiper = new Swiper(".products-slider", {
+  //         slidesPerView: 1,
+  //         initialSlide: 1,
+  //         spaceBetween: 32,
+  //         centeredSlides: true,
+  //         watchSlidesProgress: true,
+  //         loop: false,
+  //         breakpoints: {
+  //           768: {
+  //             slidesPerView: 1,
+  //             spaceBetween: 16,
+  //           },
+  //           1280: {
+  //             slidesPerView: 1,
+  //             spaceBetween: 24,
+  //           },
+  //           1440: {
+  //             slidesPerView: 1,
+  //             spaceBetween: 32,
+  //           },
+  //         },
+  //       });
+
+  //       destroySliderProducts = () => {
+  //         if (!$(".products-slider").hasClass("destroy")) {
+  //           swiper.destroy(true, true);
+  //           $(".products-slider").addClass("destroy");
+  //           $(".products-slider").find(".swiper-wrapper").removeAttr("style");
+  //         }
+  //       };
+  //     }
+  //   }
+  // }
+});
